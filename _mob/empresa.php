cb9e6a94d6d9336b544035bb550a30a167ab2109
Kenneth Rodriguez <k26rodriguez@users.noreply.github.com>
2024-03-19 14:49:13 -0600

    new files

---
+++ _mob/empresa.php
@@ -0,0 +1,125 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_header.php'; ?>
+
+<meta name="description" content="Dale un giro a tu estilo de vida viviendo en el mejor proyecto de todo Curridabat, colindamos con el área comercial más grande de toda la zona, el proyecto Aleste, agendá tu cita con nosotros y enamorate de Khaya">
+<meta name="keywords" content="Compra, Compra de apartamento, Condominios, Condominios en curridabat, Torres, Venta de apartamentos, Venta, Curridabat, concasa, Apartamentos en Torres, Apartamentos, Apartamentos en curridabat">
+<meta property="og:url" content="https://khayalatam.com" />
+<meta property="og:type" content="Web" />
+<meta property="og:title" content="Agendá tu cita con nosotros y enamorate de Khaya" />
+<meta property="og:description" content="Dale un giro a tu estilo de vida viviendo en el mejor proyecto de todo Curridabat, colindamos con el área comercial más grande de toda la zona, el proyecto Aleste, agendá tu cita con nosotros y enamorate de Khaya" />
+<meta property="og:image" content="https://khayalatam.com/khaya-mobile.webp" />
+
+<title> KHAYA - EMPRESA </title>
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_menu.php'; ?>
+	
+	<section id="info-empresa">
+	   <div class="container">
+		   <img src="/assets/images/logo-khaya-black.webp" alt="Logo Khaya S.A"> 
+			<p>Somos una empresa inmobiliaria 100% Costarricense,
+				enfocada en la construcción de vivienda en formato 
+				vertical.<br>
+				Nuestro personal cuenta con más de 20 años de 
+				experiencia en la realización de este tipo de proyectos,
+				tanto dentro como fuera del país.</p>
+		</div>
+	</section>
+	
+	
+	<section>
+		<img src="/assets/images/photo-empresa-01.webp" class="img-fluid" alt="Imagenes de Khaya - Empresa">
+	</section>
+	
+	
+	<section id="projects-logos" style="padding: 35px 10px;">
+	  <div class="container text-center">
+		<div class="row align-items-start">
+		  <div class="col-6 col-md-4 p-4">
+			<a href="#"><img src="/assets/images/curridabat-black.webp" class="img-fluid" alt="Proyecto Curridabat"></a>
+		  </div>
+		  <div class="col-6 col-md-4 p-4">
+			<img src="/assets/images/rohrmoser-black.webp" class="img-fluid" alt="Proyecto Rohrmoser">
+		  </div>
+		  <div class="col-6 col-md-4 p-4">
+			<img src="/assets/images/tibas-black.webp" class="img-fluid" alt="Proyecto Tibás">
+		  </div>
+		</div>
+	  </div>
+	</section>
+	
+	
+	<section id="images-empresa">
+			<div id="data-empresa">
+				<h3>KHAYA Curridabat</h3>
+				<h1>Último proyecto <span class="orange-k">construído</span></h1> 
+				<div class="container">
+					<div class="row">
+						<div class="col-4 ">
+							<div class="count">
+								<p class="number ngray">2</p>
+								<h4>Hectáreas</h4> 
+							</div>
+						</div>
+						<div class="col-4 ">
+							<div class="count">
+								<p class="number ndgray">5</p>
+								<h4>Torres</h4> 
+							</div>
+						</div>
+						<div class="col-4 ">
+							<div class="count">
+								<p class="number norang">280</p>
+								<h4>Apartamentos</h4> 
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	</section>  
+	
+	
+	<section id="directorio-empresa">
+		
+		<h1>Directorio <span class="orange-k">Empresarial</span></h1>
+		
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-6">
+					<a href="https://api.whatsapp.com/send?phone=50640014615"><i class="fa-brands fa-whatsapp"></i> +506 4001-4615</a>
+					<ul>
+						<li><span class="one">1</span> Ventas</li>
+						<li><span class="two">2</span> Asesoria Financiera</li>
+						<li><span class="tree">3</span> Arquitectura</li>
+					</ul>
+					
+				</div>
+				<div class="col-6">
+					<a href="tel:50640014615"><i class="fa-solid fa-phone"></i> +506 4001-4617</a>
+					<ul>
+					<ul>
+						<li><span class="one">4</span> Mercadeo</li>
+						<li><span class="two">2</span> Departamento Legal</li>
+					</ul>
+				</div>
+
+			</div>
+		</div>
+		
+		
+		
+	</section>
+
+
+	<section id="home-form" class="text-center">
+	  
+	  <script data-b24-form="inline/5/s1jvys" data-skip-moving="true">
+		 (function (w, d, u) {
+		   var s = d.createElement('script'); s.async = true; s.src = u + '?' + (Date.now() / 180000 | 0);
+		   var h = d.getElementsByTagName('script')[0]; h.parentNode.insertBefore(s, h);
+		 })(window, document, 'https://cdn.bitrix24.es/b20502967/crm/form/loader_5.js');
+	  </script>
+	
+	</section>
+	
+  <?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_footer.php'; ?>
+  
+  <script>$('.nav-url').text('Empresa');</script>
---
+++ _mob/index.php
@@ -0,0 +1,87 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_header.php'; ?>
+
+<meta name="description" content="Dale un giro a tu estilo de vida viviendo en el mejor proyecto de todo Curridabat, colindamos con el área comercial más grande de toda la zona, el proyecto Aleste, agendá tu cita con nosotros y enamorate de Khaya">
+<meta name="keywords" content="Compra, Compra de apartamento, Condominios, Condominios en curridabat, Torres, Venta de apartamentos, Venta, Curridabat, concasa, Apartamentos en Torres, Apartamentos, Apartamentos en curridabat">
+<meta property="og:url" content="https://khayalatam.com" />
+<meta property="og:type" content="Web" />
+<meta property="og:title" content="Agendá tu cita con nosotros y enamorate de Khaya" />
+<meta property="og:description" content="Dale un giro a tu estilo de vida viviendo en el mejor proyecto de todo Curridabat, colindamos con el área comercial más grande de toda la zona, el proyecto Aleste, agendá tu cita con nosotros y enamorate de Khaya" />
+<meta property="og:image" content="https://khayalatam.com/khaya-mobile.webp" />
+
+<title> KHAYA - Desarrollador inmobiliario </title>
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_menu.php'; ?>
+	
+	<section id="home-top">&nbsp;</section>
+  
+  	<section id="home-about">
+		<div class="container">
+		  <p>Somos una empresa inmobiliaria 100% Costarricense,
+			  enfocada en la construcción de vivienda en formato 
+			  vertical.</p>
+		  <p>Nuestro personal cuenta con más de 20 años de 
+			  experiencia en la realización de este tipo de proyectos,
+			  tanto dentro como fuera del país.</p>
+		</div>
+	  </section>
+    
+    <section id="projects-logos">
+      <div class="container text-center">
+        <div class="row align-items-start">
+          <div class="col-6 col-md-4 p-4">
+            <a href="#"><img src="/assets/images/curridabat-black.webp" class="img-fluid" alt="Proyecto Curridabat"></a>
+          </div>
+          <div class="col-6 col-md-4 p-4">
+            <img src="/assets/images/rohrmoser-black.webp" class="img-fluid" alt="Proyecto Rohrmoser">
+          </div>
+          <div class="col-6 col-md-4 p-4">
+            <img src="/assets/images/tibas-black.webp" class="img-fluid" alt="Proyecto Tibás">
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <section id="home-information">
+     <div class="container">
+       
+       <h1>Solicitar <span class="orange-k">Información</span></h1>
+       
+       <div class="row">
+         <div class="col-6 col-md-4 p-2">
+          <a href="https://api.whatsapp.com/send?phone=50640014615" target="_blank"> <i class="fa-brands fa-whatsapp"></i> +506 4001-4615</a>
+         </div>
+         <div class="col-6 col-md-4 p-2">
+          <a href="tel:+50640014617" target="_blank"> <i class="fa-solid fa-phone"></i> +506 4001-4617</a>
+         </div>
+         <div class="col-8 col-md-6 p-2">
+           <a href="mailto:info@khayalatam.com" target="_blank"> <i class="fa-solid fa-envelope"></i> info@khayalatam.com</a>
+          </div>
+       </div>
+       
+       <p>Curridabat, ALESTE, Torre 201, Oficina 7</p>
+       
+     </div>
+     
+     <div class="container-home-map">
+       <a href="https://www.google.com/maps/place/9%C2%B055'10.5%22N+84%C2%B001'23.7%22W/@9.919593,-84.023253,16z/data=!4m4!3m3!8m2!3d9.9195933!4d-84.0232535?hl=en&gl=US" target="_blank">
+         <img src="/assets/images/home-map.webp" class="img-fluid" alt="Mapa de Oficina Khaya S.A.">
+       </a>
+     </div> 
+
+    </section>
+    
+
+    <section id="home-form" class="text-center">
+      
+      <script data-b24-form="inline/5/s1jvys" data-skip-moving="true">
+         (function (w, d, u) {
+           var s = d.createElement('script'); s.async = true; s.src = u + '?' + (Date.now() / 180000 | 0);
+           var h = d.getElementsByTagName('script')[0]; h.parentNode.insertBefore(s, h);
+         })(window, document, 'https://cdn.bitrix24.es/b20502967/crm/form/loader_5.js');
+      </script>
+    
+    </section>
+    
+  <?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_footer.php'; ?>
+  
+  <script>$('.nav-url').text('Inicio');</script>
