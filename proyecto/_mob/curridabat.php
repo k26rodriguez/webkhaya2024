487799b89de3069d2d15bb3f24d5239b9843e2cc
Kenneth Rodriguez <k26rodriguez@users.noreply.github.com>
2024-03-19 13:22:04 -0600

    Add files via upload

---
+++ proyecto/_mob/curridabat.php
@@ -0,0 +1,274 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_header.php'; ?>
+
+<meta name="description" content="Dale un giro a tu estilo de vida viviendo en el mejor proyecto de todo Curridabat, colindamos con el área comercial más grande de toda la zona, el proyecto Aleste, agendá tu cita con nosotros y enamorate de Khaya">
+<meta name="keywords" content="Compra, Compra de apartamento, Condominios, Condominios en curridabat, Torres, Venta de apartamentos, Venta, Curridabat, concasa, Apartamentos en Torres, Apartamentos, Apartamentos en curridabat">
+<meta property="og:url" content="https://khayalatam.com" />
+<meta property="og:type" content="Web" />
+<meta property="og:title" content="Agendá tu cita con nosotros y enamorate de Khaya" />
+<meta property="og:description" content="Dale un giro a tu estilo de vida viviendo en el mejor proyecto de todo Curridabat, colindamos con el área comercial más grande de toda la zona, el proyecto Aleste, agendá tu cita con nosotros y enamorate de Khaya" />
+<meta property="og:image" content="https://khayalatam.com/Khaya-Web.jpg" />
+
+ <!-- Owl Carousel Assets -->
+<link href="/assets/css/owl.carousel.css" rel="stylesheet">
+<link href="/assets/css/owl.theme.css" rel="stylesheet">
+
+<title> KHAYA CURRIDABAT </title>
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_menu.php'; ?>
+
+<section id="project-header">
+	<img src="/assets/images/header-curridabat.webp" class="img-fluid" alt="Khaya - Curridabat">
+</section>
+
+<section id="project-location">
+	<h1>Ubicación</h1>
+	<img src="/assets/images/mapa-proyecto-curridabat.webp" class="map-project" alt="Ubicación Khaya Curridabat">
+	<span class="nameproject-black">Curridabat</span>
+	<p>San José, Curridabat, Granadilla, Costado Norte de la UACA,
+		contiguo a Aleste</p>
+</section>
+
+<section id="project-master">
+	<h1>Master <span class="orange-k">Plan</span></h1>
+	<img src="/assets/images/master-plan-curridabat.webp" class="img-fluid" alt="Master plan - Khaya Curridabat">
+</section>
+
+<section id="project-type">
+	<h1>Planta <span class="orange-k">Tipo</span></h1>
+	<img src="/assets/images/planta-tipo-curridabat.webp" class="img-fluid" alt="Planta tipo - Khaya Curridabat">
+</section>
+
+<section id="project-apto">
+	
+	
+	<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+	  <li class="nav-item" role="presentation">
+		<button class="nav-link active" id="pills-one-tab" data-bs-toggle="pill" data-bs-target="#pills-one" type="button" role="tab" aria-controls="pills-one" aria-selected="true">Tipo 1</button>
+	  </li>
+	  <li class="nav-item" role="presentation">
+		<button class="nav-link" id="pills-two-tab" data-bs-toggle="pill" data-bs-target="#pills-two" type="button" role="tab" aria-controls="pills-two" aria-selected="false">Tipo 2</button>
+	  </li>
+	  <li class="nav-item" role="presentation">
+		<button class="nav-link" id="pills-tree-tab" data-bs-toggle="pill" data-bs-target="#pills-tree" type="button" role="tab" aria-controls="pills-tree" aria-selected="false">Tipo 3</button>
+	  </li>
+	  <li class="nav-item" role="presentation">
+		<button class="nav-link" id="pills-four-tab" data-bs-toggle="pill" data-bs-target="#pills-four" type="button" role="tab" aria-controls="pills-four" aria-selected="false">Tipo 4</button>
+	  </li>
+	</ul>
+	<div class="tab-content" id="pills-tabContent">
+	  <div class="tab-pane fade show active" id="pills-one" role="tabpanel" aria-labelledby="pills-one-tab" tabindex="0">
+		  
+		  <!-- START TAB -->
+		  
+		  <div class="container">
+			  <div class="row">
+				  <div class="col-6 box-fix-a">
+					  <span class="mt-projects">75m<sup>2</sup></span>
+					  <h3><span class="orange-k">3</span> Habitaciones</h3>
+				  </div>
+				  <div class="col-6 box-fix-a">
+					  <h5 class="box-fix-b">Desde:</h5>
+					  <h3>$135.000 usd<sup>*</sup></h3>
+				  </div>
+			  </div>
+		  </div>	  
+		  
+		  <div id="pic-full-slider">
+			  	<img id="imagechange" src="/assets/images/isometrico-01.webp" class="image-change img-fluid" alt="Planta tipo - Khaya Curridabat">
+		  </div>
+		  
+		  <div id="owl-demo" class="owl-demo">
+					
+			<div class="item"><img class="link" src="/assets/images/tumb-isometrico-01.webp" alt="Owl Image"></div>
+			<div class="item"><img class="link" src="/assets/images/tumb-isometrico-02.webp" alt="Owl Image"></div>
+			<div class="item"><img class="link" src="/assets/images/tumb-isometrico-01.webp" alt="Owl Image"></div>
+			<div class="item"><img class="link" src="/assets/images/tumb-isometrico-02.webp" alt="Owl Image"></div>
+			<div class="item"><img class="link" src="/assets/images/tumb-isometrico-01.webp" alt="Owl Image"></div>
+		   
+		  </div>
+		  
+		 <div class="container mt-5">
+		   <div class="row">
+			   <div class="col-4"><i class="icon-rooms"></i> 1 Parqueo</div>
+			   <div class="col-4"><i class="icon-bath"></i> 2 Baños</div>
+			   <div class="col-4"><i class="icon-park"></i> 3 Habitaciones</div>
+		   </div>
+		 </div>
+		  
+		  <button type="button" class="btn btngray-khaya mt-5 mb-5">Vista 360º</button>
+		  
+		  <!-- END TAB -->
+		  
+	  </div>
+	  <div class="tab-pane fade" id="pills-two" role="tabpanel" aria-labelledby="pills-two-tab" tabindex="0">
+		 
+		  
+		  
+		 <!-- START TAB --> 
+		  
+		   <div class="container">
+			   <div class="row">
+				   <div class="col-6 box-fix-a">
+					   <span class="mt-projects">70m<sup>2</sup></span>
+					   <h3><span class="orange-k">3</span> Habitaciones</h3>
+				   </div>
+				   <div class="col-6 box-fix-a">
+					   <h5 class="box-fix-b">Desde:</h5>
+					   <h3>$125.000 usd<sup>*</sup></h3>
+				   </div>
+			   </div>
+		   </div>	  
+		   
+		   <div id="pic-full-slider">
+				   <img id="imagechange" src="/assets/images/isometrico-01.webp" class="image-change img-fluid" alt="Planta tipo - Khaya Curridabat">
+		   </div>
+		   
+		   <div id="owl-demo" class="owl-demo">
+					 
+			 <div class="item"><img class="link" src="/assets/images/tumb-isometrico-01.webp" alt="Owl Image"></div>
+			 <div class="item"><img class="link" src="/assets/images/tumb-isometrico-02.webp" alt="Owl Image"></div>
+			 <div class="item"><img class="link" src="/assets/images/tumb-isometrico-01.webp" alt="Owl Image"></div>
+			 <div class="item"><img class="link" src="/assets/images/tumb-isometrico-02.webp" alt="Owl Image"></div>
+			 <div class="item"><img class="link" src="/assets/images/tumb-isometrico-01.webp" alt="Owl Image"></div>
+			
+		   </div>
+		   
+		  <div class="container mt-5 text-center">
+			<div class="row">
+				<div class="col-4">1 Parqueo</div>
+				<div class="col-4">2 Baños</div>
+				<div class="col-4">3 Habitaciones</div>
+			</div>
+		  </div>
+		   
+		   <button type="button" class="btn btngray-khaya mt-5 mb-5">Vista 360º</button>
+		   
+		   <!-- END TAB -->
+		   
+	  
+	  
+	  </div>
+	  <div class="tab-pane fade" id="pills-tree" role="tabpanel" aria-labelledby="pills-tree-tab" tabindex="0">
+		  
+		  
+		  
+		 <!-- START TAB -->  
+		  
+			<div class="container">
+				<div class="row">
+					<div class="col-6 box-fix-a">
+						<span class="mt-projects">66m<sup>2</sup></span>
+						<h3><span class="orange-k">2</span> Habitaciones</h3>
+					</div>
+					<div class="col-6 box-fix-a">
+						<h5 class="box-fix-b">Desde:</h5>
+						<h3>$122.500 usd<sup>*</sup></h3>
+					</div>
+				</div>
+			</div>	  
+			
+			<div id="pic-full-slider">
+					<img id="imagechange" src="/assets/images/isometrico-01.webp" class="image-change img-fluid" alt="Planta tipo - Khaya Curridabat">
+			</div>
+			
+			<div id="owl-demo" class="owl-demo">
+					  
+			  <div class="item"><img class="link" src="/assets/images/tumb-isometrico-01.webp" alt="Owl Image"></div>
+			  <div class="item"><img class="link" src="/assets/images/tumb-isometrico-02.webp" alt="Owl Image"></div>
+			  <div class="item"><img class="link" src="/assets/images/tumb-isometrico-01.webp" alt="Owl Image"></div>
+			  <div class="item"><img class="link" src="/assets/images/tumb-isometrico-02.webp" alt="Owl Image"></div>
+			  <div class="item"><img class="link" src="/assets/images/tumb-isometrico-01.webp" alt="Owl Image"></div>
+			 
+			</div>
+			
+		   <div class="container mt-5">
+			 <div class="row">
+				 <div class="col-4">1 Parqueo</div>
+				 <div class="col-4">2 Baños</div>
+				 <div class="col-4">2 Habitaciones</div>
+			 </div>
+		   </div>
+			
+			<button type="button" class="btn btngray-khaya mt-5 mb-5">Vista 360º</button>
+			
+		  <!-- END TAB -->
+	  
+	  
+	  
+	  </div>
+	  <div class="tab-pane fade" id="pills-four" role="tabpanel" aria-labelledby="pills-four-tab" tabindex="0">
+		  
+		  
+		  
+		 <!-- START TAB -->  
+		  
+			<div class="container">
+				<div class="row">
+					<div class="col-6 box-fix-a">
+						<span class="mt-projects">56m<sup>2</sup></span>
+						<h3><span class="orange-k">1</span> Habitaciones</h3>
+					</div>
+					<div class="col-6 box-fix-a">
+						<h5 class="box-fix-b">Desde:</h5>
+						<h3>$102.500 usd<sup>*</sup></h3>
+					</div>
+				</div>
+			</div>	  
+			
+			<div id="pic-full-slider">
+					<img id="imagechange" src="/assets/images/isometrico-01.webp" class="image-change img-fluid" alt="Planta tipo - Khaya Curridabat">
+			</div>
+			
+			<div id="owl-demo" class="owl-demo">
+					  
+			  <div class="item"><img class="link" src="/assets/images/tumb-isometrico-01.webp" alt="Owl Image"></div>
+			  <div class="item"><img class="link" src="/assets/images/tumb-isometrico-02.webp" alt="Owl Image"></div>
+			  <div class="item"><img class="link" src="/assets/images/tumb-isometrico-01.webp" alt="Owl Image"></div>
+			  <div class="item"><img class="link" src="/assets/images/tumb-isometrico-02.webp" alt="Owl Image"></div>
+			  <div class="item"><img class="link" src="/assets/images/tumb-isometrico-01.webp" alt="Owl Image"></div>
+			 
+			</div>
+			
+		   <div class="container mt-5">
+			 <div class="row">
+				 <div class="col-4">1 Parqueo</div>
+				 <div class="col-4">1.5 Baños</div>
+				 <div class="col-4">1 Habitaciones</div>
+			 </div>
+		   </div>
+			
+			<button type="button" class="btn btngray-khaya mt-5 mb-5">Vista 360º</button>
+			
+		  <!-- END TAB -->
+	  
+	  
+	  
+	  </div>
+	</div>
+	
+	
+	
+</section>
+
+<section id="project-form" class="p-4">
+	
+	<script data-b24-form="inline/5/s1jvys" data-skip-moving="true">
+		 (function (w, d, u) {
+		   var s = d.createElement('script'); s.async = true; s.src = u + '?' + (Date.now() / 180000 | 0);
+		   var h = d.getElementsByTagName('script')[0]; h.parentNode.insertBefore(s, h);
+		 })(window, document, 'https://cdn.bitrix24.es/b20502967/crm/form/loader_5.js');
+	  </script>
+	  
+</section>
+
+
+
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_footer.php'; ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<script src="/assets/js/owl.carousel.js"></script>
+
+<script>$('.nav-url').text('KHAYA Curridabat');</script>
+
---
+++ proyecto/_mob/launion.php
@@ -0,0 +1,21 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_header.php'; ?>
+
+<meta name="description" content="Dale un giro a tu estilo de vida viviendo en el mejor proyecto de todo Curridabat, colindamos con el área comercial más grande de toda la zona, el proyecto Aleste, agendá tu cita con nosotros y enamorate de Khaya">
+<meta name="keywords" content="Compra, Compra de apartamento, Condominios, Condominios en curridabat, Torres, Venta de apartamentos, Venta, Curridabat, concasa, Apartamentos en Torres, Apartamentos, Apartamentos en curridabat">
+<meta property="og:url" content="https://khayalatam.com" />
+<meta property="og:type" content="Web" />
+<meta property="og:title" content="Agendá tu cita con nosotros y enamorate de Khaya" />
+<meta property="og:description" content="Dale un giro a tu estilo de vida viviendo en el mejor proyecto de todo Curridabat, colindamos con el área comercial más grande de toda la zona, el proyecto Aleste, agendá tu cita con nosotros y enamorate de Khaya" />
+<meta property="og:image" content="https://khayalatam.com/Khaya-Web.jpg" />
+
+<title> KHAYA LA UNION </title>
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_menu.php'; ?>
+
+
+xxxxxx
+
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_footer.php'; ?>
+
+<script>$('.nav-url').text('KHAYA La Unión');</script>
