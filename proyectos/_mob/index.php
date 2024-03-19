d5b9079a7ede94ffc1a27fb6aa37c215a8127d42
Kenneth Rodriguez <k26rodriguez@users.noreply.github.com>
2024-03-19 13:20:13 -0600

    Add files via upload

---
+++ proyectos/_mob/index.php
@@ -0,0 +1,85 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_header.php'; ?>
+<meta name="description" content="Dale un giro a tu estilo de vida viviendo en el mejor proyecto de todo Curridabat, colindamos con el área comercial más grande de toda la zona, el proyecto Aleste, agendá tu cita con nosotros y enamorate de Khaya">
+<meta name="keywords" content="Compra, Compra de apartamento, Condominios, Condominios en curridabat, Torres, Venta de apartamentos, Venta, Curridabat, concasa, Apartamentos en Torres, Apartamentos, Apartamentos en curridabat">
+<meta property="og:url" content="https://khayalatam.com" />
+<meta property="og:type" content="Web" />
+<meta property="og:title" content="Agendá tu cita con nosotros y enamorate de Khaya" />
+<meta property="og:description" content="Dale un giro a tu estilo de vida viviendo en el mejor proyecto de todo Curridabat, colindamos con el área comercial más grande de toda la zona, el proyecto Aleste, agendá tu cita con nosotros y enamorate de Khaya" />
+<meta property="og:image" content="https://khayalatam.com/Khaya-Web.jpg" />
+
+<title> PROYECTOS KHAYA </title>
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_menu.php'; ?>
+  
+	<section id="proyectos-gallery">
+		  <div id="bar-top-gray">
+		  	<h1>Galería <span class="orange-k">de Proyectos</span></h1>  
+		  </div> 
+		  
+		   <div class="row-int">
+				
+					<div class="img-project text-center">
+					   <div id="curridabat" class="roundedkhaya">
+						  <div id="shadowimag">   
+					  		<img src="/assets/images/logo-curridabat.webp" class="img-logo-pro" alt="Khaya Curridabat">
+					   		 <a href="/proyecto/khaya-curridabat" class="btn btn-link btn-khaya">Conocer proyecto</a>
+					   		<span class="nameproject">Curridabat</span>
+					      </div>
+					   </div>
+					</div>
+
+				
+					<div class="img-project text-center">
+					   <div id="rohrmoser" class="roundedkhaya">
+						  <div id="shadowimag">   
+							  <img src="/assets/images/logo-rohrmoser.webp" class="img-logo-pro" alt="Khaya Rohrmoser">
+								<a href="/proyecto/khaya-rohrmoser" class="btn btn-link btn-khaya">Conocer proyecto</a>
+							   <span class="nameproject">Rohrmoser</span>
+						  </div>
+					   </div>
+					</div>
+					
+					<div class="img-project text-center">
+					   <div id="tibas" class="roundedkhaya">
+						  <div id="shadowimag">   
+							  <img src="/assets/images/logo-tibas.webp" class="img-logo-pro" alt="Khaya Tibás">
+								<a href="/proyecto/khaya-tibas" class="btn btn-link btn-khaya">Conocer proyecto</a>
+							   <span class="nameproject">Tibás</span>
+						  </div>
+					   </div>
+					</div>
+					
+					<!--<div class="img-project text-center">
+					   <div id="tibas" class="roundedkhaya">
+						  <div id="shadowimag">   
+							  <img src="/assets/images/logo-tibas.webp" class="img-logo-pro" alt="Khaya Tibás">
+								<a href="/proyecto/khaya-tibas" class="btn btn-link btn-khaya">Conocer proyecto</a>
+							   <span class="nameproject">Tibás</span>
+						  </div>
+					   </div>
+					</div>
+									
+				
+				
+					<div class="img-project text-center">
+					   <div id="sabia" class="roundedkhaya">
+						  <a href="/proyecto/khaya-sabia" class="btn btn-link btn-khaya">Conocer proyecto</a>
+					   </div>
+					</div>-->
+				
+				
+					<div class="img-project text-center">
+					   <div id="proyecto-nuevo" class="roundedkhaya">
+						  <a href="/proyecto/" class="btn btn-link btn-khaya">Conocer proyecto</a>
+					   </div>
+					</div>
+				
+		   </div>
+		   
+		   
+			
+	</section>
+
+  <?php include_once $_SERVER['DOCUMENT_ROOT'].'/tpl_mob_footer.php'; ?>
+  
+  <script>$('.nav-url').text('Proyectos');</script>
---
+++ proyectos/index.php
@@ -0,0 +1,34 @@
+<?php
+    header('Expires: Thu, 1 Jan 1970 00:00:00 GMT');
+    header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+    header('Cache-Control: post-check=0, pre-check=0',false);
+    header('Pragma: no-cache');
+    
+// detect mobile device
+function isMobileDevice() {
+	return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+}
+	
+function isMobileDev(){
+    if(isset($_SERVER['HTTP_USER_AGENT']) and !empty($_SERVER['HTTP_USER_AGENT'])){
+       $user_ag = $_SERVER['HTTP_USER_AGENT'];
+       if(preg_match('/(Mobile|Android|GoBrowser|[0-9]x[0-9]*|uZardWeb\/|Mini|Doris\/|Skyfire\/|iPhone|Fennec\/|Maemo|Iris\/|CLDC\-|Mobi\/)/uis',$user_ag)){
+          return true;
+       };
+    };
+    return false;
+}
+
+
+// Use the function
+if(isMobileDev()){
+    // Do something for only mobile users
+   // include './_mob/index.php';
+    include $_SERVER['DOCUMENT_ROOT'].'/proyectos/_mob/index.php';
+}
+else {
+    // Do something for only desktop users
+   // include './_desk/index.php';
+    include $_SERVER['DOCUMENT_ROOT'].'/proyectos/_desk/index.php';
+}
+?>
