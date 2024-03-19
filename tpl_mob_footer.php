97cb8820653fcb14f550bbd35f17043a0dd922f1
Kenneth Rodriguez <k26rodriguez@users.noreply.github.com>
2024-03-19 13:19:34 -0600

    Add files via upload

---
+++ tpl_mob_footer.php
@@ -0,0 +1,15 @@
+    <footer class="footer">
+         <ul class="social-footer">
+            <li><a href="https://www.instagram.com/khayalatam/" target="_blank" aria-label="Redes Social, Instagram de Khaya S.A."><i class="fa-brands fa-instagram"></i></a></li>
+            <li><a href="https://www.facebook.com/khayalatam" target="_blank" aria-label="Redes Social, Facebook de Khaya S.A."><i class="fa-brands fa-facebook-f"></i></a></li>
+            <li><a href="https://www.tiktok.com/@khayalatam?_t=8kHOctjQzpO&_r=1" target="_blank" aria-label="Redes Social, Tiktok de Khaya S.A."><i class="fa-brands fa-tiktok"></i></a></li>
+         </ul>
+         &copy; <span id="yearpage"></span> Khaya S.A. All Rights Reserved.
+      </footer>
+
+
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+     <script src="/assets/js/khaya.script.min.js"></script>
+    
+    </body>
+</html>
---
+++ tpl_mob_menu.php
@@ -0,0 +1,73 @@
+    <link rel='stylesheet' href='/assets/css/styles.css'>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <script src="https://kit.fontawesome.com/61b36392d5.js" crossorigin="anonymous"></script>
+
+</head>
+
+    <body data-spy="scroll" data-target=".navbar" data-offset="90">
+
+   <!-- START HEADER -->
+     <header>
+          <!--Navigation-->
+          <nav class="navbar navbar-top-default navbar-expand-lg navbar-simple nav-line">
+             <div class="container">
+                <a href="/" title="Logo" class="logo">
+                   <!--Logo Default-->
+                   <img src="/assets/images/logo-icon-white.webp" alt="logo" class="ml-lg-3 m-0">
+                </a>
+                <span class="nav-url">INICIO</span>
+                <!--Nav Links-->
+             </div>
+             <!--Side Menu Button-->
+             <div class="navigation-toggle">
+                <ul class="slider-social toggle-btn my-0">
+                   <li>
+                      <a href="javascript:void(0);" class="sidemenu_btn" id="sidemenu_toggle">
+                      <span></span>
+                      <span></span>
+                      </a>
+                   </li>
+                </ul>
+             </div>
+          </nav>
+          <!--Side Nav-->
+          <div class="side-menu">
+             
+             <div class="inner-wrapper">
+                <nav class="side-nav w-100">
+                   <ul class="navbar-nav text-capitalize">
+                      <li class="nav-item">
+                          <a class="nav-link" href="/">INICIO</a>
+                       </li>
+                      <li class="nav-item">
+                         <a class="nav-link" href="/proyectos/">PROYECTOS</a>
+                      </li>
+                      <li class="nav-item">
+                         <a class="nav-link pro-m" href="/proyecto/khaya-curridabat">- KHAYA Curridabat</a>
+                      </li>
+                      <li class="nav-item">
+                         <a class="nav-link pro-m" href="/proyecto/khaya-la-union">- KHAYA La Unión</a>
+                      </li>
+                      <li class="nav-item">
+                         <a class="nav-link pro-m" href="/proyecto/khaya-rohrmoser">- KHAYA Rohrmoser</a>
+                      </li>
+                      <li class="nav-item">
+                         <a class="nav-link pro-m" href="/proyecto/khaya-tibas">- KHAYA Tibás</a>
+                      </li>
+                      <li class="nav-item">
+                         <a class="nav-link pro-m" href="/proyecto/khaya-sabia">- KHAYA Savia</a>
+                      </li>
+                      <li class="nav-item">
+                         <a class="nav-link" href="/empresa">EMPRESA</a>
+                      </li>
+                   </ul>
+                </nav>
+            
+             </div>
+          </div>
+          <a id="close_side_menu" href="javascript:void(0);"></a>
+          <!-- End side menu -->
+       </header>
+     <!-- END HEADER -->  
